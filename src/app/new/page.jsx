9739ee6910e8c612aa96f73e0c0f7ca24477d616
Samikshya-bmj <bomzansamikshya@gmail.com>
2026-06-27 13:29:19 +0545

    Modified by samikshya

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function newPage() {
-    return (
-      
-      <h1 className='h-screen grid place-content-center text-5xl text-red-600'>This is new page</h1>
+  return (
+
+    <h1 className='h-screen grid place-content-center text-5xl text-red-600'>This is new page changed by samikhya</h1>
   )
 }
 
