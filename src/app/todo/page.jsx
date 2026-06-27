e53a906c51261dc4217bcc8e58add15c59226943
shailesh-00git <sy468698@gmail.com>
2026-06-27 12:47:41 +0545

    Added the todo  list

---
+++ src/app/todo/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function todo() {
+  return (
+      <div>
+          <h1>Todo List</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo ipsum voluptates officia libero! Eligendi veritatis ipsa animi. Tenetur laborum minus similique? Deleniti, numquam aliquam? Quod similique dolorum est debitis quos?</p>
+    </div>
+  )
+}
+
+export default todo
